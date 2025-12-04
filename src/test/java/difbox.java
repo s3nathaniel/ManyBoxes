a94e5e2fb3192a73ffb1805a733b549e6cf59582
Dmitrii Polizhai <s3nathaniels@gmail.com>
2025-12-04 19:00:51 +0300

    init commit

---
+++ src/test/java/difbox.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class difbox {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void dbox(){
+        open("/automation-practice-form");
+    }
+}
